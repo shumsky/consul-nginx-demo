f6accf08b13ab387f8d4a8d3abe35d34c97475bd
Anton Shumsky <anton.shumsky@gmail.com>
2017-01-11 12:33:13 +0200

    Implement app.js in node-demo.

---
+++ node-demo/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const msb = require('msb');
+const consul = require('consul')();
+
+const NAMESPACE = 'service:discovery:demo';
+
+const app = express();
+
+app.get('/test', (req, resp) => {
+    msb.request(NAMESPACE, 'hello', (err, payload) => {
+        if (err) {
+            return resp.status(500).end();
+        }
+        resp.status(200).json(payload);
+    });
+});
+
+app.listen(3000).on('listening', () => {
+    consul.agent.service.register({name: 'node-demo'});
+});
